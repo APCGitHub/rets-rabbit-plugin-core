89ea3b6dfce6b3efde28b9a091823a0e5bf7f00c
Andrew Clinton <ddrew.clinton@gmail.com>
2018-01-03 13:45:32 -0500

    Add providers config

---
+++ src/config/providers.php
@@ -0,0 +1,22 @@
+<?php 
+
+return array(
+	'rets_rabbit' => array(
+		'resources' => array(
+			'media' 		=> 'media',
+			'members' 		=> null,
+			'offices' 		=> null,
+			'openhouses' 	=> 'open-house',
+			'properties' 	=> 'property',
+		),
+	),
+	'trestle' => array(
+		'resources' => array(
+			'media' 		=> 'Media',
+			'members' 		=> 'Member',
+			'offices' 		=> 'Office',
+			'openhouses' 	=> null,
+			'properties' 	=> 'Property',
+		),
+	),
+);
