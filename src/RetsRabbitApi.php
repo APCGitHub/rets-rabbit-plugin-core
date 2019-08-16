827ef99d1913dcc2b0dc0d38b28ecd0201671cc7
Andrew Clinton <ddrew.clinton@gmail.com>
2019-08-15 22:37:44 -0400

    Bootstrap open house trait

--- src/RetsRabbitApi.php
+++ src/RetsRabbitApi.php
@@ -6,12 +6,14 @@ namespace Apc\RetsRabbit\Core;
 
 use Apc\RetsRabbit\Core\Clients\RetsRabbitClient;
 use Apc\RetsRabbit\Core\Traits\AccessTokenResourceTrait;
+use Apc\RetsRabbit\Core\Traits\OpenHouseResourceTrait;
 use Apc\RetsRabbit\Core\Traits\PropertyResourceTrait;
 
 class RetsRabbitApi
 {
     use PropertyResourceTrait,
-        AccessTokenResourceTrait;
+        AccessTokenResourceTrait,
+        OpenHouseResourceTrait;
 
     /**
      * @var RetsRabbitClient
